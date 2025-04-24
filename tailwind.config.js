f4611ea19a610581ea6d58cabc05054c68e31a26
Fereshteakbari <fereshteakbariam18@gmail.com>
2025-04-24 15:10:30 +0330

    final

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+// module.exports = {
+//   content: ["./*.html", "./src/**/*.js"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: "#ff510c",
+//         lightGray: "#fafafa",
+//       },
+//       fontFamily: {
+//         sans: ["Yekan", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
